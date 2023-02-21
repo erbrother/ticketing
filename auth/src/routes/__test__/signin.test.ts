4f4aca3bbfe31f793b2a3fbe508b1a6f35dca7f2
zsx01015229 <zsx01015229@alibaba-inc.com>
2023-02-21 14:06:57 +0800

    feat: chapter 10 test auth

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,49 @@
+import requset from "supertest";
+import { app } from "../../app";
+it("fails when a email that does not exist is supplied", async () => {
+  await requset(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("fails when an incorrect password is supplied", async () => {
+  await requset(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await requset(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "asdasd",
+    })
+    .expect(400);
+});
+
+it("responds with a cookie when given valid credentials", async () => {
+  await requset(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const response = await requset(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
